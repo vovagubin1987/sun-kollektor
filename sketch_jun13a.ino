4b693f4da957b6818a200ab32017fcd052509ff4
vovagubin1987 <vovagubin1987@yandex.ru>
2023-06-23 23:58:34 +0300

    Add files via upload

---
+++ sketch_jun13a.ino
@@ -0,0 +1,268 @@
+#include "zaglavie.h"
+
+void WIFIinit() {
+  
+
+  // Попытка подключения к точке доступа
+  WiFi.mode(WIFI_STA);
+  byte tries = 11;
+  
+  if (_ssid == "" && _password == "") {
+    WiFi.begin();
+  }
+  else {
+    WiFi.begin(_ssid.c_str(), _password.c_str());
+  }
+  // Делаем проверку подключения до тех пор пока счетчик tries
+  // не станет равен нулю или не получим подключение
+  while (--tries && WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+    
+  }
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    // Если не удалось подключиться запускаем в режиме AP
+    Serial.println("");
+    Serial.println("WiFi up AP");
+    StartAPMode();
+  }
+  else {
+    // Иначе удалось подключиться отправляем сообщение
+    // о подключении и выводим адрес IP
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    //jsonWrite(configJson, "ip", WiFi.localIP().toString()); 
+  }
+}
+
+bool StartAPMode() {
+  IPAddress apIP(192, 168, 177, 1);
+  IPAddress staticGateway(192, 168, 177, 1);
+  IPAddress staticSubnet(255, 255, 255, 0);
+  //jsonWrite(configJson, "ip", apIP.toString()); 
+  // Отключаем WIFI
+  WiFi.disconnect();
+  // Меняем режим на режим точки доступа
+  WiFi.mode(WIFI_AP);
+  // Задаем настройки сети
+  WiFi.softAPConfig(apIP, staticGateway, staticSubnet);
+  //Включаем DNS
+  dnsServer.start(53, "*", apIP);
+  // Включаем WIFI в режиме точки доступа с именем и паролем
+  // хронящихся в переменных _ssidAP _passwordAP
+  //String _ssidAP = jsonRead(configSetup, "ssidAP");
+  //String _passwordAP = jsonRead(configSetup, "passwordAP");
+  WiFi.softAP(_ssidAP.c_str(), _passwordAP.c_str());
+  return true;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+  delay(7);
+  Serial.println("Start 1-WIFI");
+  //Запускаем WIFI
+  WIFIinit();
+
+
+  ds.begin();//инициализация 18b20
+  ds.setResolution(sensor_tOutK,10);//18b20 установка разрешения преобразования
+  ds.setResolution(sensor_tOutN,10);//18b20 установка разрешения преобразования
+
+
+  server.on("/search", [](){
+    /*
+    if(!server.authenticate(www_username, www_password))
+      return server.requestAuthentication();
+      */
+      //getDeviceAddress();
+      //_18b20search();
+      //printAddress;
+      String message="<head></head><body>";
+      message+="<table>";
+      for( i = 0; i < 2; i++) {
+        ds.getAddress(Thermometer, i);
+        byte t[8];
+        t[i]= ds.getTempCByIndex(i);
+        delay(200);
+        message+=tr_td;
+        message+="id=";
+        message+=i;
+        message+="|"+_td_td;
+        //message+="<tr><td>";
+        for (uint8_t ii = 0; ii < 8; ii++)
+        {
+          // zero pad the address if necessary
+          message+=Thermometer[ii];
+          message+="#";
+        }
+        //message+=Thermometer;
+        //message+="nnn";
+        //message+=addr[i];
+        //message+="mmm";
+        //message+=t[i];
+        message+="|"+_td_td+"|"+_td_td;
+        //const uint8_t tmp4=addr[i];
+        //DeviceAddress tmp4={addr[i]};
+        //message+=ds.getTempC(tmp4);
+        message+=t[i];
+        //message+="</td></tr>";
+        message+=_td__tr;
+        //message+="</br>";
+  };
+      message+="</table>";
+      message+="</body></html>";
+      server.send(200, "text/html", message);
+  });
+
+server.on("/", [](){
+    /*
+    if(!server.authenticate(www_username, www_password))
+      return server.requestAuthentication();
+      */
+      signed char schet2;
+      //schet2=schet;
+      //if (schet==0){
+      //  schet2=1;
+      //}
+      String message="<!doctype html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n<meta http-equiv=\"refresh\" content=\"30;/\"> \n</head>\n";
+    message+="<body>\n<div style=\"position:absolute;left:20px;top:19px;width:250px;height:24px;z-index:0;\">\n<span style=\"color:#000000;font-family:Arial;font-size:21px;\">";
+    
+      message+="t выхода из коллектора:";
+      message+=String(g_tOutK);
+      message+="\n t выхода из накопителя:";
+      message+=String(g_tOutN);
+      message+="\n накоплено энергии итого:";
+      message+=String(g_Watt);
+      message+="\n за время:";
+      message+=String(g_Time/g_TOpros);
+      message+="\n статус циркуляционного насоса:";
+      message+=String(g_Nasos);
+      message+="\n t принудительного отключения:";
+      message+=String(g_TBreakOff);
+      //message+="\n";
+     // message+=String(schet2);
+      //message+="\n";
+      //message+=String(kof);
+      //message+="\n";
+      //message+=String(tmp1);
+      //message+="\n";
+      //message+=String(rew);
+      //message+="\n";
+      //message+=String(razgon_tmp);
+      //message+="\n";
+      //message+=String(razgon);
+      //message+="\n";
+      //message+=String(ryj);
+     // message+="\n";
+     // message+=String(tBreakOffK);
+     // message+="\n";
+     // message+=String(tBreakOffIn);
+     // message+="\n";
+      //message+=String(tDeltaKCritical);
+      ////message+="\n";
+      //message+=String(tDeltaIn);
+     // message+="\n";
+      //message+=String(tOutCritical);
+      //message+="\n";
+      //message+=String(tDeltaKCritical_economy);
+     // message+="\n";
+     // message+=String(razgon);
+     // message+="\n";
+     // message+=String(negative);
+     // message+="\n";
+      //message+=String(zalp_per);
+      message+="\n";
+      message+="</span></div>\n</body>\n</html>";
+      server.send(200, "text/html", message);
+  });
+
+
+
+
+//запускаем сервер
+server.begin();
+
+
+ts.add(0, g_TOpros, [&](void*) { // Запустим задачу 0 с интервалом test
+      signed short  tmp_tOutK=0;
+  signed short tmp_tOutN=0;
+
+  if (indexforindex==0){
+  if (ds.isConnected(sensor_tOutK)){
+    //#define ETS_INTR_LOCK() ets_intr_lock() //запрет прерываний
+  ds.requestTemperaturesByAddress(sensor_tOutK);
+  //delay(200);
+  tmp_tOutK= (short) ds.getTempC(sensor_tOutK);
+  
+  //#define ETS_INTR_UNLOCK() ets_intr_unlock() //разрешение всех прерываний
+  }
+
+  if (ds.isConnected(sensor_tOutN)){
+    //#define ETS_INTR_LOCK() ets_intr_lock() //запрет прерываний
+  ds.requestTemperaturesByAddress(sensor_tOutN);
+  //
+  delay(200);
+  tmp_tOutN= (short) ds.getTempC(sensor_tOutN);
+  
+  //#define ETS_INTR_UNLOCK() ets_intr_unlock() //разрешение всех прерываний
+  }
+
+  g_Time=g_Time+1;
+  signed short dt=0;
+
+
+
+  if (g_Time<2){
+    g_tOutN=tmp_tOutN+273;
+    g_tOutK=tmp_tOutK+273;
+
+  } else {
+    signed short tmp1=0;
+    signed short tmp2=0;
+    tmp1=(g_tOutN+tmp_tOutN+273)/2;
+    tmp2=(g_tOutK+tmp_tOutN+273)/2;
+    g_tOutN=tmp1;
+    g_tOutK=tmp2;
+
+  };
+
+  dt=g_tOutK-g_tOutN;
+  if (dt<0){
+    dt=0;
+  };
+  g_Watt=g_Watt+g_C*dt*g_M*g_TOpros/60/10;
+
+  if (dt<3){
+    g_Nasos=false;
+    digitalWrite(rele, LOW);
+  } else {
+    digitalWrite(rele, HIGH);
+    g_Nasos=true;
+  }
+  if (g_Watt> 10446744073709 or g_Time>10446744073709){
+    Serial.println("Resetting ESP");
+      ESP.restart();
+  }
+
+  }
+
+
+    }, nullptr, true);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  ts.update();
+  server.handleClient();
+  yield();
+
+}
---
+++ zaglavie.h
@@ -0,0 +1,81 @@
+
+#include <ESP8266WiFi.h>        
+#include <ESP8266WebServer.h>   
+#include <ESP8266SSDP.h>       
+#include <FS.h>                 
+#include <ArduinoJson.h>        //Установить из менеджера библиотек. https://arduinojson.org/
+//                   
+#include <ESP8266HTTPUpdateServer.h>  
+#include <DNSServer.h> 
+#include "TickerScheduler.h"       //https://github.com/Toshik/TickerScheduler Видео с уроком
+#include <WebSocketsServer.h>    //https://github.com/Links2004/arduinoWebSockets 
+// Библиотеки устройств
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <math.h> 
+#include <stdlib.h> 
+
+#define debug true // вывод отладочных сообщений
+#define PIN18B20   4   // 18b20 пин
+#define rele   5   // рэле пин
+DeviceAddress sensor_tOutK = {0x28, 0xFC, 0xD8, 0x49, 0xF6, 0x8A, 0x3C, 0x8B};
+DeviceAddress sensor_tOutN = {0x28, 0xB3, 0x22, 0x49, 0xF6, 0xE0, 0x3C, 0x7C};
+// Объект для обнавления с web страницы
+ESP8266HTTPUpdateServer httpUpdater;
+
+// Web интерфейс для устройства
+ESP8266WebServer server(80);
+
+// Для файловой системы
+File fsUploadFile;
+
+// Для работы символьных имен в режиме AP
+DNSServer dnsServer;
+
+//Планировщик задач (Число задач)
+TickerScheduler ts(2);
+
+// Объект для  webSocket
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+// Датчик DHT
+//DHT dht;
+//#define dhtPin 4
+
+//global
+signed short g_tOutK=0; //температура на выходе из коллектора
+signed short g_tOutN=0; //температура на выходе из накопителя
+byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+
+OneWire oneWire(PIN18B20); // вход датчиков 18b20
+DallasTemperature ds(&oneWire);//как ds
+OneWire ds1(PIN18B20);//как ds1 для поиска датчиков
+
+
+const char index_html[] PROGMEM={"<!doctype html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>Web Server</title>\n<meta name=\"generator\" content=\"WYSIWYG Web Builder 12 - http://www.wysiwygwebbuilder.com\">\n<link href=\"Untitled1.css\" rel=\"stylesheet\">\n<link href=\"index.css\" rel=\"stylesheet\">\n</head>\n<body>\n<div id=\"wb_Text1\" style=\"position:absolute;left:34px;top:41px;width:570px;height:23px;z-index:0;\">\n<span style=\"color:#000000;font-family:Arial;font-size:20px;\">TURN ON:</span></div>\n<div id=\"wb_Text2\" style=\"position:absolute;left:34px;top:85px;width:570px;height:23px;z-index:1;\">\n<span style=\"color:#000000;font-family:Arial;font-size:20px;\">TURN OFF:</span></div>\n<input type=\"submit\" id=\"Button1\" onclick=\"window.location.href=' ./on';return false;\" name=\"\" value=\"Submit\" style=\"position:absolute;left:146px;top:40px;width:96px;height:25px;z-index:2;\">\n<input type=\"submit\" id=\"Button2\" onclick=\"window.location.href='./off';return false;\" name=\"\" value=\"Submit\" style=\"position:absolute;left:146px;top:84px;width:96px;height:25px;z-index:3;\">\n</body>\n</html>"};
+String ip = "";
+const String tr_td="<tr><td>";
+const String _td_td="</td><td>";
+const String _td__tr="</td><tr>";
+const int g_TBreakOff=85;
+bool g_Nasos=false;
+unsigned long g_Watt=0;
+unsigned long g_Time=0;
+const unsigned long g_TOpros=180;
+byte indexforindex = 0;
+
+float t[] = {0, 0, 0, 0, 0, 0};
+
+String _ssid="";
+String _password="";
+
+String _ssidAP="sun_kollektor_v1";
+String _passwordAP="sun_kollektor_v1";
+const int g_M=10;
+const int g_C=42;
+
+
+DeviceAddress Thermometer;
